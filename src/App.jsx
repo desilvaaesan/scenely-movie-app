6209c043eba7431c29e8bfa741bda7288730b6d7
Aesan De Silva <aesanclyde.desilva@gmail.com>
2025-07-06 01:07:03 +0800

    feat: implement search

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Search from './components/Search'
 import Spinner from './components/Spinner';
 import { useEffect, useState } from 'react'
 
-const API_BASE_URL = 'https://api.themoviedb.org/3/discover/movie';
+const API_BASE_URL = 'https://api.themoviedb.org/3';
 
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
@@ -21,14 +21,14 @@ const App = () => {
   const [movieList, setMovieList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchMovies = async () => {
+  const fetchMovies = async (query = '') => {
     setIsLoading(true);
     setErrorMessage('');
 
 
 
     try {
-      const endpoint = `${API_BASE_URL}?sort_by=popularity.desc`;
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
       const response = await fetch(endpoint, API_OPTIONS);
 
@@ -55,8 +55,8 @@ const App = () => {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, []);
+    fetchMovies(searchTerm);
+  }, [searchTerm]);
 
   return (
     <main>
